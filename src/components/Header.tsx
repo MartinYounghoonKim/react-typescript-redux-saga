7a2d0a54ac010c9e01c78481a2c375c7d9ac666b
martin <martin@tmon.co.kr>
2018-12-03 20:18:52 +0900

    Sampling Header's component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
-export default class Header extends React.Component {
+interface IState {
+  newTodo: string;
+}
+
+export default class Header extends React.Component<{}, IState> {
   private inputElement: HTMLInputElement;
+  readonly state: IState = {
+    newTodo: ''
+  };
 
   createTodoItem = (e: React.KeyboardEvent<HTMLInputElement>) => {
     const ENTER_KEYCODE = 13;
     const value = this.inputElement.value;
     const isAbleAddStatus = e.keyCode === ENTER_KEYCODE && value.length > 0;
-
+    this.setState({
+      newTodo: e.currentTarget.value
+    });
     if (isAbleAddStatus) {
       e.currentTarget.value = '';
     }
@@ -17,12 +26,10 @@ export default class Header extends React.Component {
     return (
       <header>
         <h1 className="todo-app__header">todos</h1>
-        <input
-          type="text"
-          className="todo-app__new-todo"
-          onKeyDown={this.createTodoItem}
-          ref={(inputElement: HTMLInputElement) => this.inputElement = inputElement}
-        />
+        <input type="text"
+               className="todo-app__new-todo"
+               onKeyDown={this.createTodoItem}
+               ref={(inputElement: HTMLInputElement) => this.inputElement = inputElement}/>
         <button/>
       </header>
     )
