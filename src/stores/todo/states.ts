c7a25ad3a917e9b9854d79ce107b14d34b65e677
martinyounghoonkim <hoons0131@gmail.com>
2018-12-31 23:23:02 +0900

    Apply immutable library in todo state

--- src/stores/todo/states.ts
+++ src/stores/todo/states.ts
@@ -1,4 +1,28 @@
+import {Record, Map, List} from "immutable";
 import {ITodo} from "interfaces/models";
+
+export const TodoItemRecord = Record({
+  id: "",
+  text: "",
+  isDone: "",
+});
+
+export class TodoItem extends TodoItemRecord {
+  constructor () {
+    super();
+  }
+}
+
+export const TodoStateRecord = Record({
+  editingId: '',
+  todos: List(),
+});
+
+export class TodoState extends TodoStateRecord {
+  readonly todos: List<TodoItem>
+}
+
+const test = new TodoState();
 
 interface ITodoStates {
   editingId: string;
